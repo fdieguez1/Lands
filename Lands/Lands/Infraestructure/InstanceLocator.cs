f8d2410aa9f22bbce727b2ba222a8b098cf2b1bc
Fernando Dieguez <fernandodieguez@hotmail.com.ar>
2018-12-10 17:50:42 -0300

    Progreso hasta parte 006 del tutorial

---
+++ Lands/Lands/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infraestructure
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
