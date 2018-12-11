e355728c902aca2b1d088e4d95c94247d7f2d6c2
Fernando Dieguez <fernandodieguez@hotmail.com.ar>
2018-12-11 14:18:48 -0300

    Agregados viewmodels y bindeados tutorial, parte 007

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        private String password;
+
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        private bool isRunning;
+
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set { isRunning = value; }
+        }
+        private bool isRemembered;
+        public bool IsRemembered
+        {
+            get { return isRemembered; }
+            set { isRemembered = value; }
+        }
+        #endregion
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        public ICommand LoginCommand {
+            get;
+            set;
+        }
+        public ICommand RegisterCommand
+        {
+            get;
+            set;
+        }
+    }
+}
