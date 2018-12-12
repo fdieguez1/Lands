97c4119f33e464eab06cf7947a735edded0e3f65
Fernando Dieguez <fernandodieguez@hotmail.com.ar>
2018-12-12 14:03:32 -0300

    Part008 del curso de xamarin INotifyPropertyChanged

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,61 +1,118 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Lands.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : INotifyPropertyChanged
     {
-        #region Properties
-        
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+        #region Attributes
         private string email;
+        private String password;
+        private bool isRunning;
+        private bool isEnabled;
+        private bool isRemembered;
+        #endregion
+
+        #region Properties
+
 
         public string Email
         {
             get { return email; }
             set { email = value; }
         }
-        private String password;
+        
 
         public String Password
         {
             get { return password; }
             set { password = value; }
         }
-        private bool isRunning;
+        
 
         public bool IsRunning
         {
             get { return isRunning; }
             set { isRunning = value; }
         }
-        private bool isRemembered;
+        
+
         public bool IsRemembered
         {
             get { return isRemembered; }
             set { isRemembered = value; }
+        }
+
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set { isEnabled = value; }
         }
         #endregion
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = false;
         }
         #endregion
 
         #region Commands
-
-        #endregion
-
-        public ICommand LoginCommand {
-            get;
-            set;
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
         }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an e-mail",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password",
+                    "Accept");
+                return;
+            }
+            if (this.Email != "jzuluaga55@gmail.com" || this.Password != "1234")
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Fuck yeaah",
+                    "Accept");
+            return;
+
+        }
+
         public ICommand RegisterCommand
         {
             get;
-            set;
         }
+        #endregion
     }
 }
